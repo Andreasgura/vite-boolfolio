a1259e6faf1b315a0b1d10a87d7ae044a68e84d2
Andrea Sgura <andre-56@hotmail.it>
2024-06-12 17:11:09 +0200

    (AllProjects) pagination

--- src/pages/AllProjects.vue
+++ src/pages/AllProjects.vue
@@ -1,20 +1,20 @@
 <template>
-    <h1>Tutti i progetti</h1>
-    <div class="container">
-        <div class="row">
-            <CardComponent v-for="project in projects" :key="project.id" :project="project"/>
-        </div>
+  <h1>Tutti i progetti</h1>
+  <div class="container">
+    <div class="row">
+      <CardComponent v-for="project in projects" :key="project.id" :project="project" />
+    </div>
 
 
     <nav aria-label="Page navigation example">
-  <ul class="pagination">
-    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item"><a class="page-link" href="#">Next</a></li>
-  </ul>
-</nav>
+      <ul class="pagination">
+        <li class="page-item"><a class="page-link" :class="{ 'disabled' : currentPage == 1 }" href="#" @click.prevent="setParams(currentPage - 1)">Previous</a></li>
+
+        <li class="page-item" v-for="page in lastpage" :key="page"><a class="page-link" :class="{ 'active' : currentPage == page }" href="#" @click.prevent="setParams(page)">{{ page }}</a></li>
+
+        <li class="page-item"><a class="page-link" :class="{ 'disabled' : currentPage == lastpage }" href="#" @click.prevent="setParams(currentPage + 1)">Next</a></li>
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -23,29 +23,42 @@ import axios from 'axios';
 import CardComponent from '../components/CardComponent.vue';
 import { store } from '../store';
 export default {
-    name: 'AllProjects',
-    components: {
-        CardComponent
-    },
-    data() {
+  name: 'AllProjects',
+  components: {
+    CardComponent
+  },
+  data() {
     return {
       store,
       projects: [],
       currentPage: 1,
+      lastpage: 0,
+      params: {}
     }
   },
   methods: {
+    setParams(number) {
+      this.currentPage = number
+      console.log(this.currentPage);
+      this.params = {
+        page : this.currentPage
+      }
+      console.log(this.params.page);
+      this.getProjects();
+    },
     getProjects() {
       axios
         .get(this.store.baseUrl + this.store.endPoints.projects, {
-          params: {
-            page: this.currentPage
-          }
+          params: this.params
         })
         .then((res) => {
-          console.log(res.data);
+          // console.log(res.data);
           this.projects = res.data.results.data;
-          console.log(this.projects);
+          // console.log(this.projects);
+          this.currentPage = res.data.results.current_page;
+          // console.log(this.currentPage);
+          this.lastpage = res.data.results.last_page;
+          this.params = null
         });
     }
   },
@@ -55,6 +68,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
